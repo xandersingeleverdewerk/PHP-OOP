bc733f36815ab076bd28261168151c3a1d4a2f35
xandersingeleverdewerk <47180650+xandersingeleverdewerk@users.noreply.github.com>
2019-03-22 12:40:26 +0100

    Add files via upload

---
+++ project5/src/Schooltrip.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace project5;
+
+class Schooltrip
+{
+    private $name;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return "The name of the park = ".$this->name;
+    }
+}
---
+++ project5/src/Student.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace project5;
+
+class Student extends Person
+{
+    private $payMoney;
+    private $paid;
+    private $classname = [];
+
+    public function setPayMoney($payMoney)
+    {
+        $this->payMoney = $payMoney;
+    }
+
+    public function setPaid($paid)
+    {
+        $this->paid = $paid;
+    }
+
+    public function getPaid()
+    {
+        if ($this->paid < $this->payMoney)
+        {
+            return "you dont have paid enough money";
+        } elseif ($this->paid > $this->payMoney)
+        {
+            $this->paid = $this->paid - ($this->paid - $this->payMoney);
+            return "the money paid = €".$this->paid;
+        }
+        return $this->paid;
+    }
+
+    public function addClassname($classname)
+    {
+        $this->classname[] = $classname;
+    }
+
+    public function getClassname()
+    {
+        $print = "";
+        foreach ($this->classname as $classname)
+        {
+            $print = $classname->getName();
+        }
+        return $print;
+    }
+}
