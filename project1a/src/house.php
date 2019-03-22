6a6279670ca177e3b55aaa9b25bd4b9fc329f00a
xandersingeleverdewerk <47180650+xandersingeleverdewerk@users.noreply.github.com>
2019-03-22 14:13:58 +0100

    Update house.php

--- project1a/src/house.php
+++ project1a/src/house.php
@@ -13,82 +13,52 @@ class House
 
     public function setNumberOfFloors($number_of_floors)
     {
-        if ($number_of_floors < 0)
-        {
-            print("A house need to contain at least a storage level.");
-        }else {
-            print ("Number of floors = ".$this->number_of_floors);
-            $this->number_of_floors = $number_of_floors;
-        }
+        $this->number_of_floors = $number_of_floors;
     }
 
     public function getNumberOfFloors()
     {
-        return $this->number_of_floors;
+        return "numbers of floors = ".$this->number_of_floors;
     }
 
     public function setNumberOfRooms($number_of_rooms)
     {
-        if ($number_of_rooms < 0)
-        {
-            print("A house must contain at least a hallway.");
-        }else {
-            print ("Number of rooms = ".$this->number_of_rooms);
-            $this->number_of_rooms = $number_of_rooms;
-        }
+        $this->number_of_rooms = $number_of_rooms;
     }
 
     public function getNumberOfRooms()
     {
-        return $this->number_of_rooms;
+        return "numbers of rooms = ".$this->number_of_rooms;
     }
 
     public function setWidth($width)
     {
-        if ($width < 1.5)
-        {
-            print("A house have to be at least 1.5m width.");
-        }else{
-            print ("Width = ".$this->width);
-            $this->width = $width;
-        }
+        $this->width = $width;
     }
 
     public function getWidth()
     {
-        return $this->width;
+        return "width = ".$this->width;
     }
 
     public function setDepth($depth)
     {
-        if ($depth < 4)
-        {
-            print("A house have to be at least 4m depth");
-        }else{
-            print ("Depth = ".$this->depth);
-            $this->depth = $depth;
-        }
+        $this->depth = $depth;
     }
 
     public function getDepth()
     {
-        return $this->depth;
+        return "depth = ".$this->depth;
     }
 
     public function setHeight($height)
     {
-        if ($height < 2.40)
-        {
-            print("A house have to be at least 2.40m height.");
-        }else{
-            print ("Height = ".$this->height);
-            $this->height = $height;
-        }
+        $this->height = $height;
     }
 
     public function getHeight()
     {
-        return $this->height;
+        return "height = ".$this->height;
     }
 
     public function setPrice($price)
@@ -96,15 +66,9 @@ class House
         $this->price = $price;
     }
 
-    public function calculatePrice()
+    public function getPrice()
     {
         $this->price = $this->price * ($this->width * $this->height * $this->depth);
-        if ($this->price == 0)
-        {
-            print("One of the attributes is incorrect");
-        }else {
-            print "Price = " . $this->price;
-
-        }
+        return "Price = ".$this->price;
     }
 }
